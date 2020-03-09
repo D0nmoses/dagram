4c5983b52426e15ee7b8d17aa9e352d9e46a3480
Don Moses <githinjidon@gmail.com>
2020-03-09 11:19:09 +0300

    add .gitignore and .env with environment variables

--- dagram/settings.py
+++ dagram/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 from django.urls import reverse_lazy
@@ -132,7 +133,15 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Configure Django App for Heroku.
+django_heroku.settings(locals())
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
