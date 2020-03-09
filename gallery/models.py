a6ee9cf67c8871077f60d0cf40cd2561eb25f4bf
Don Moses <githinjidon@gmail.com>
2020-03-09 07:59:18 +0300

    add likes class that defines likes for a post as well as class method get_post_likes to return he likes and num_likes that sums the number of likes and returns the number

--- gallery/models.py
+++ gallery/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
-# import datetime as dt
+import datetime as dt
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-# from django.db.models import Sum
+from django.db.models import Sum
 
 
 # Create your models here.
@@ -161,3 +161,31 @@ class Comment(models.Model):
         post_comments = Comment.objects.filter(post=post_id)
 
         return post_comments
+
+class Like(models.Model):
+    '''	
+    Class that define the likes a post gets	
+    '''
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+
+    post = models.ForeignKey(Post,on_delete=models.CASCADE)
+
+    likes_number = models.PositiveIntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+    @classmethod
+    def get_post_likes(cls,post_id):
+
+        post_likes = Like.objects.filter(post=post_id)
+
+        return post_likes
+
+    @classmethod
+    def num_likes(cls,post_id):
+
+        post = Like.objects.filter(post=post_id)
+        found_likes = post.aggregate(Sum('likes_number')).get('likes_number__sum',0)
+
+        return found_likes
