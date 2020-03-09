0e9b58690d63c47db7bd374acd06703f496a4d40
Don Moses <githinjidon@gmail.com>
2020-03-09 07:36:57 +0300

    define modal structure to display on image click

--- gallery/urls.py
+++ gallery/urls.py
@@ -4,5 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
-
+    re_path(r'^$', views.home, name='home'),
 ]
+# if settings.DEBUG:
+#    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
