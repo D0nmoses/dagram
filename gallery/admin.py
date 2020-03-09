26d2846a2a36431e9f1659aedf940eca77c39854
Don Moses <githinjidon@gmail.com>
2020-03-09 08:42:32 +0300

    register models in admins.py

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Like, Follow, Post, Profile, Tag, Comment
 
-# Register your models here.
+
+admin.site.register(Like)
+admin.site.register(Follow)
+admin.site.register(Post)
+admin.site.register(Profile)
+admin.site.register(Tag)
+admin.site.register(Comment)
