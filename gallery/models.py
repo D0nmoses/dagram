1d48638f663f8d0c7b4b91024450e41ccd062899
Don Moses <githinjidon@gmail.com>
2020-03-09 07:45:39 +0300

    add class tags with methods for saving, deleting and getting tags

--- gallery/models.py
+++ gallery/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
-import datetime as dt
+# import datetime as dt
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.db.models import Sum
+# from django.db.models import Sum
 
 
 # Create your models here.
@@ -55,3 +55,36 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
+
+
+class Tag(models.Model):
+    '''	
+    Class that defines categories of posts and tags on posts	
+    '''
+    name = models.CharField(max_length=30, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    def save_tag(self):
+        '''	
+        Method to save a new tag to the database	
+        '''
+        self.save()
+
+    def delete_tag(self):
+        '''	
+        Method to delete a tag from the database	
+        '''
+        self.delete()
+
+    @classmethod
+    def get_tags(cls):
+        '''	
+        Method that gets all tags from the database	
+        Returns:	
+            gotten_tags : list of tag objects from the database	
+        '''
+        gotten_tags = Tag.objects.all()
+
+        return gotten_tags
