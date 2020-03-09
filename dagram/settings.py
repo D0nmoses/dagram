6ac6dc1bbf9effa23a24b91b2df514f4129e3987
Don Moses <githinjidon@gmail.com>
2020-03-09 08:30:19 +0300

    add MEDIA ROOT and MEDIA_URL to settings.py

--- dagram/settings.py
+++ dagram/settings.py
@@ -129,4 +129,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+# Examples: "http://example.com/media/", "http://media.example.com/"
+MEDIA_URL = '/media/'
