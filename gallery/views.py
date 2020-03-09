9be4f7e53037cc2de74d7da4a209bc31432d2c11
Don Moses <githinjidon@gmail.com>
2020-03-09 10:35:37 +0300

    add view profile to views.py and create template profile.html

--- gallery/views.py
+++ gallery/views.py
@@ -1,11 +1,15 @@
+from django.http import Http404,
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Follow, Like, Comment, Post, Profile
+from django.core.exceptions import ObjectDoesNotExist
+
 # Create your views here.
 
 @login_required(login_url='/accounts/login')
 def home(request):
     current_user = request.user
+
 
     title = 'Home'
 
@@ -13,7 +17,7 @@ def home(request):
 
     posts = Post.get_posts()
 
-    comments = Comment.get_post_comments(posts.id)
+    # comments = Comment.get_post_comments()
 
     following_posts = []
 
@@ -24,5 +28,25 @@ def home(request):
             if follow.profile == post.profile:
                 following_posts.append(post)
 
-    return render(request, 'all-gram/home.html',
-                  {"comments":comments, "title": title, "following": following, "user": current_user, "posts": posts, "following_posts": following_posts})
+    return render(request, 'all_gram/home.html',
+                  { "title": title, "following": following, "user": current_user, "posts": posts, "following_posts": following_posts})
+
+@login_required(login_url='/accounts/login')
+def profile(request,id):
+    '''	
+    View function to display the profile of the logged in user when they click on the user icon	
+    '''
+    current_user = request.user
+
+    try:
+
+        single_profile = Profile.objects.get(user=current_user.id)
+
+        title = f'{current_user.username}\'s'
+
+        posts = Post.objects.filter(user=current_user.id)
+
+        return render(request, 'all-posts/my-profile.html', {"title":title,"current_user":current_user,"posts":posts})
+
+    except ObjectDoesNotExist:
+        raise Http404()
