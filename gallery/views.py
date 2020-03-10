6f072b94846922256322a1caaea3b61d672353c1
Don Moses <githinjidon@gmail.com>
2020-03-10 05:49:47 +0300

    add profiles attribute to home view to allow displaying of other profiles on home page

--- gallery/views.py
+++ gallery/views.py
@@ -29,8 +29,27 @@ def home(request):
             if follow.profile == post.profile:
                 following_posts.append(post)
 
+    # to allow us to view other user's profiles
+
+
+    profiles = Profile.get_other_profiles(current_user.id)
+
+    following = Follow.objects.filter(user=current_user)
+
+    following_profile_list = []
+
+    for follow in following:
+        following_profile_list.append(follow.profile)
+
+    profiles_list = []
+
+    for profile in profiles:
+
+        if profile not in following_profile_list:
+            profiles_list.append(profile)
+
     return render(request, 'all_gram/home.html',
-                  { "title": title, "following": following, "user": current_user, "posts": posts, "following_posts": following_posts})
+                  { "title": title, "following": following, "user": current_user, "posts": posts, "following_posts": following_posts, "profiles":profiles_list})
 
 @login_required(login_url='/accounts/login')
 def profile(request,id):
